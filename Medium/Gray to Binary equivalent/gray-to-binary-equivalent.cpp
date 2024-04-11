7260d4671f1b0dd1bbec1da3a561eecba44ed737
Shekhar Patil <121214166+Shekharp-45@users.noreply.github.com>
2024-04-11 15:43:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Gray to Binary equivalent/gray-to-binary-equivalent.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        
+         int ans = 0;
+        while(n > 0)//till n>=0
+        {
+            ans = ans ^ n;//xor
+            n = n >> 1;//right shift
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+
+// } Driver Code Ends
