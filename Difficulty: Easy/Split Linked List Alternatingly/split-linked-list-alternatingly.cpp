c70640f60f39a5ee30b09e7a8851df41402442ee
Shekhar Patil <121214166+Shekharp-45@users.noreply.github.com>
2024-10-17 13:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function to split a linked list into two lists alternately
+    vector<Node*> alternatingSplitList(struct Node* head) {
+        // Your code here
+        struct Node* curr1 = head;
+    struct Node* curr2 = head->next;
+    struct Node* head1 = curr1;
+    struct Node* head2 = curr2;
+
+    struct Node* ans1 = head1;
+    struct Node* ans2 = head2;
+    
+    vector<Node*> ans;
+    
+    while (curr1 != NULL && curr2 != NULL) {
+        if (curr1->next != NULL) curr1->next = curr1->next->next;
+        curr1 = curr1->next;
+        
+        if (curr2 != NULL && curr2->next != NULL) curr2->next = curr2->next->next;
+        curr2 = curr2->next;
+    }
+    
+    return {ans1, ans2};
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        struct Node* head = new Node(arr[0]);
+        struct Node* tail = head;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        vector<Node*> result = ob.alternatingSplitList(head);
+        printList(result[0]);
+        printList(result[1]);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
