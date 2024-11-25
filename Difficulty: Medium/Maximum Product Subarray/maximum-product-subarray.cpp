2164a8b00db76c10db19d76a508cc583baadf446
Shekhar Patil <121214166+Shekharp-45@users.noreply.github.com>
2024-11-25 21:26:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -3,26 +3,27 @@
 
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
 
-	// Function to find maximum product subarray
-	long long maxProduct(vector<int> arr, int n) {
-	  long long product = INT_MIN;
-      long long pref = 1;
-      long long suff = 1;
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+      int n=arr.size();
+      int product = INT_MIN;
+      int pref = 1;
+      int suff = 1;
       for(int i=0;i<n;i++)
       {
-        if(pref == 0)   pref =1;
-        if(suff == 0)  suff =1;
+        if(pref == 0) pref =1;
+        if(suff == 0) suff =1;
         pref = pref * arr[i];
         suff = suff * arr[n-i-1];
         product = max(product , max(pref,suff));
       }
     return product;
-	}
+    }
 };
 
 //{ Driver Code Starts.
@@ -30,16 +31,31 @@ public:
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n, i;
-        cin >> n;
-        vector<int> arr(n);
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
         Solution ob;
-        auto ans = ob.maxProduct(arr, n);
+        auto ans = ob.maxProduct(arr);
         cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
