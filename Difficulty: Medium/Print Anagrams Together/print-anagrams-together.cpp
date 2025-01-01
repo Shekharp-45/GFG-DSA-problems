56daf202ecb7ba0f45417b27ef7ddf3db7e60730
Shekhar Patil <121214166+Shekharp-45@users.noreply.github.com>
2025-01-01 22:46:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Anagrams Together/print-anagrams-together.cpp
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.cpp
@@ -1,17 +1,17 @@
 //{ Driver Code Starts
-//Initial Template for C++
+// Initial Template for C++
 #include <bits/stdc++.h>
 #include <unordered_map>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
+// User function Template for C++
 
-class Solution{
+class Solution {
   public:
-    vector<vector<string> > Anagrams(vector<string>& string_list) {
-        vector<vector<string>> ans;
+    vector<vector<string>> anagrams(vector<string>& string_list) {
+ vector<vector<string>> ans;
         map<string , vector<string>> res;
         string temp;
         for(int i=0; i<string_list.size(); i++){
@@ -28,28 +28,33 @@ class Solution{
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<string> string_list(n);
-        for (int i = 0; i < n; ++i)
-            cin>>string_list[i]; 
-        Solution ob;
-        vector<vector<string> > result = ob.Anagrams(string_list);
-        sort(result.begin(),result.end());
-        for (int i = 0; i < result.size(); i++)
-        {
-            for(int j=0; j < result[i].size(); j++)
-            {
-                cout<<result[i][j]<<" ";
-            }
-            cout<<"\n";
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<string> arr;
+        string input_line;
+        getline(cin, input_line);
+        stringstream ss(input_line);
+        string word;
+        while (ss >> word) {
+            arr.push_back(word);
         }
+
+        Solution ob;
+        vector<vector<string> > result = ob.anagrams(arr);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
     }
 
     return 0;
