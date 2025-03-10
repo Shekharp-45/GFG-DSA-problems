6da6e3b8335de5f74afbe4b5be0c4f86dfda4a9f
Shekhar Patil <121214166+Shekharp-45@users.noreply.github.com>
2025-03-10 22:17:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -1,43 +1,59 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int t[101][101];//2D coz i & j are dynamic
-    int solve(string &s1, string &s2 , int i, int j){
-        if(i==s1.size())return s2.size()-j;//base case cross answer dene
-        if(j==s2.size())return s1.size()-i;
-        int ans=INT_MAX;
-        //cheak repeated or not
-        if(t[i][j]!=-1){
-            return t[i][j];
+    int func(string s1,string s2,int i,int j,vector<vector<int>>&dp){
+        if(i==s1.size() && j==s2.size()){
+            return 0;
         }
-        if(s1[i]!=s2[j]){
-            ans=min({ans,solve(s1,s2,i+1,j)+1,solve(s1,s2,i,j+1)+1,solve(s1,s2,i+1,j+1)+1});
-        }else{
-            return t[i][j]=solve(s1,s2,i+1,j+1);//same asel tr next la jane without adding +1 i.e ans
+        if(i==s1.size()){
+            return s2.size()-j;
         }
-        return t[i][j]=ans;
+        if(j==s2.size()){
+            return s1.size()-i;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return dp[i][j]=func(s1,s2,i+1,j+1,dp);
+        }
+        else{
+            return dp[i][j]=min(1+func(s1,s2,i+1,j+1,dp),min(1+func(s1,s2,i,j+1,dp),1+func(s1,s2,i+1,j,dp)));
+        }
     }
-    int editDistance(string s1, string s2) {
-        memset(t,-1,sizeof(t));
-        return solve(s1,s2,0,0);
+    int editDistance(string& s1, string& s2) {
+        int n=s1.size(),m=s2.size();
+        if(s1==s2)return 0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return func(s1,s2,0,0,dp);
     }
 };
 
+
+
 //{ Driver Code Starts.
+
 int main() {
+
     int T;
     cin >> T;
+    cin.ignore();
     while (T--) {
-        string s, t;
-        cin >> s >> t;
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
         Solution ob;
-        int ans = ob.editDistance(s, t);
+        int ans = ob.editDistance(s1, s2);
         cout << ans << "\n";
+        cout << "~" << endl;
     }
     return 0;
 }
